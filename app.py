3a9288a86a1ec099180e340598a865cc6eb32002
Mart van Buren <mart.van.buren@gmail.com>
2017-11-10 02:32:08 +0000

    Some form fixes

--- app.py
+++ app.py
@@ -23,41 +23,44 @@ def index():
 
 @app.route('/a5/trend1')
 def trend1():
-    subCollectionName = request.args.get('subCollectionName', default="authors", type=str)
-    venue = request.args.get('venue', default="arXiv", type=str)
-    yearMin = request.args.get('yearMin', default=2000, type=int)
-    yearMax = request.args.get('yearMax', default=2017, type=int)
+    subCollectionName = request.args.get('subCollectionName', type=str) or "authors"
+    venue = request.args.get('venue', type=str) or "arXiv"
+    yearMin = request.args.get('yearMin', type=int) or 2000
+    yearMax = request.args.get('yearMax', type=int) or 2017
     return controller.trend1(subCollectionName, venue, yearMin, yearMax)
 
 
 @app.route('/a5/trend2')
 def trend2():
-    subCollectionName = request.args.get('subCollectionName', default="authors", type=str)
-    venues = request.args.get('venues', default="arXiv,Journal of abnormal child psychology", type=str).split(",")
-    year = request.args.get('year', default=2000, type=int)
+    subCollectionName = request.args.get('subCollectionName', type=str) or "authors"
+    venues = request.args.get('venues', type=str) or "arXiv,Journal of abnormal child psychology"
+    venues = venues.split(",")
+    year = request.args.get('year', type=int) or 2000
     return controller.trend2(subCollectionName, venues, year)
 
 
 @app.route('/a5/trend3')
 def trend3():
-    n = request.args.get('n', default=10, type=int)
-    elementType = request.args.get('elementType', default="authors", type=str)
-    filterKeys = request.args.get('filterKeys', default="venue", type=str).split(",")
-    filterValues = request.args.get('filterValues', default="ICSE", type=str).split(",")
+    n = request.args.get('n', type=int) or 10
+    elementType = request.args.get('elementType', type=str) or "authors"
+    filterKeys = request.args.get('filterKeys', type=str) or "venue"
+    filterKeys = filterKeys.split(",")
+    filterValues = request.args.get('filterValues', type=str) or "ICSE"
+    filterValues = filterValues.split(",")
     return controller.trend3(n, elementType, filterKeys, filterValues)
 
 
 @app.route('/a5/trend4')
 def trend4():
-    author = request.args.get('author', default="Lin Li", type=str)
-    group = request.args.get('group', default="arXiv", type=str)
+    author = request.args.get('author', type=str) or "Lin Li"
+    group = request.args.get('group', type=str) or "arXiv"
     return controller.trend4(author, group)
 
 
 @app.route('/a5/trend5')
 def trend5():
     title = request.args.get('title', type=str) or "Low-density parity check codes over GF(q)"
-    maxDepth = request.args.get('maxDepth', type=int) or 2
+    maxDepth = request.args.get('maxDepth', type=int) or 1
     return controller.trend5(title, maxDepth)
 
 
