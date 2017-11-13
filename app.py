2ab38bd9a639432f5bf0e5f8c67b151f635a8c10
Apoorva Ullas <apoorva_ullas@yahoo.com.sg>
2017-11-13 11:48:37 +0800

    Remove more unused code

--- app.py
+++ app.py
@@ -63,35 +63,3 @@ def trend5():
     maxDepth = request.args.get('maxDepth', type=int) or 1
     return controller.trend5(title, maxDepth)
 
-
-# -------------------------------------------------------------------------
-#   A4 Questions
-# -------------------------------------------------------------------------
-
-@app.route('/a4/q1')
-def question1():
-    return controller.question1()
-
-
-@app.route('/a4/q2')
-def question2():
-    return controller.question2()
-
-
-@app.route('/a4/q3')
-def question3():
-    return controller.question3()
-
-
-@app.route('/a4/q4')
-def question4():
-    return controller.question4()
-
-
-@app.route('/a4/q5')
-def question5():
-    return controller.question5()
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
