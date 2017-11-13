76bafa6916f8793204dd492e522c6ee473cbaaff
Apoorva Ullas <apoorva_ullas@yahoo.com.sg>
2017-11-13 11:47:05 +0800

    Remove unused code

--- controller.py
+++ controller.py
@@ -84,37 +84,3 @@ class Controller(object):
             edges=edges,
             form_function="getFormTrend5",
         )
-
-    # -------------------------------------------------------------------------
-    #   A4 Questions
-    # -------------------------------------------------------------------------
-
-    def question1(self):
-        self.model.loadJsonFile()
-        data = self.model.getTopAuthByVenue(10, "arXiv")
-        view = self.jinja_env.get_template('views/question1.html')
-        return view.render(data=data)
-
-    def question2(self):
-        data = self.model.getPaperMostCited(5, "arXiv")
-        view = self.jinja_env.get_template('views/question2.html')
-        return view.render(data=data)
-
-    def question3(self):
-        self.model.loadJsonFile()
-        data = self.model.getAmountPublicationPerYear("ICSE")
-        view = self.jinja_env.get_template('views/question3.html')
-        return view.render(data=data)
-
-    def question4(self):
-        self.model.loadJsonFile()
-        nodes, edges = self.model.getCitationTreeByPaper(
-            "Low-density parity check codes over GF(q)", 2)
-        view = self.jinja_env.get_template('views/question4.html')
-        return view.render(nodes=nodes, edges=edges)
-
-    def question5(self):
-        self.model.loadJsonFile()
-        data = self.model.getNumberOfTimeCitedPerYear(2000, 2016, "Yoshua Bengio")
-        view = self.jinja_env.get_template('views/question5.html')
-        return view.render(data=data)
